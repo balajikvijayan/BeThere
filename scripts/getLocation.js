826ec3b86b16d1bb2460417e54d0f68c7b3ff9e1
balajikvijayan <balaji.k.vijayan@gmail.com>
2014-05-30 18:19:58 -0500

    v1.03

--- scripts/getLocation.js
+++ scripts/getLocation.js
@@ -1,5 +1,5 @@
-var map;
-var infowindow;
+var map, infowindow, GeoMarker;
+var markersArray = [];
 
 function getLocation(){
       if (navigator.geolocation)
@@ -33,14 +33,19 @@ function initialize(position) {
 
     map = new google.maps.Map(document.getElementById("map-canvas"),
               mapOptions);
+    GeoMarker = new GeolocationMarker(map);
+    GeoMarker.setCircleOptions({fillColor: '#808080'});
 
-    var request = {
-    		    location: myLocation,
-    		    radius: 500
-    		  };
+    google.maps.event.addListenerOnce(GeoMarker, 'position_changed', function() {
+      map.setCenter(myLocation);  
+    });
+
+    google.maps.event.addListener(GeoMarker, 'geolocation_error', function(e) {
+      alert('There was an error obtaining your position. Message: ' + e.message);
+    });
+
+    GeoMarker.setMap(map);
     infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
     
     var input = /** @type {HTMLInputElement} */(
     	      document.getElementById('pac-input'));
@@ -49,36 +54,25 @@ function initialize(position) {
     	    /** @type {HTMLInputElement} */(input));
     google.maps.event.addListener(searchBox, 'places_changed', function() {
         var places = searchBox.getPlaces();
+        clearOverlays();
 
-        for (var i = 0, marker; marker = markers[i]; i++) {
-          marker.setMap(null);
-        }
-
-        // For each place, get the icon, place name, and location.
         markers = [];
         var bounds = new google.maps.LatLngBounds();
         for (var i = 0, place; place = places[i]; i++) {
-          var image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25)
-          };
-
-          // Create a marker for each place.
-          var marker = new google.maps.Marker({
-            map: map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location
-          });
-
-          markers.push(marker);
-
-          bounds.extend(place.geometry.location);
+        	var marker = new google.maps.Marker({
+        	    map: map,
+        	    position: place.geometry.location
+        	  });
+        	  markersArray.push(marker);
+              bounds.extend(place.geometry.location);
+              var placeName = place.name.toString();
+        	  google.maps.event.addListener(marker, 'click', (function(placeName) {
+        		return function() {
+        			infowindow.setContent(placeName);
+        			infowindow.open(map, this);
+        		}
+        	  })(placeName));
         }
-
         map.fitBounds(bounds);
       });
     google.maps.event.addListener(map, 'bounds_changed', function() {
@@ -87,34 +81,11 @@ function initialize(position) {
       });
 }
 
-function callback(results, status) {
-	  if (status == google.maps.places.PlacesServiceStatus.OK) {
-	    for (var i = 0; i < results.length; i++) {
-	      createMarker(results[i]);
-	    }
+function clearOverlays() {
+	  for (var i = 0; i < markersArray.length; i++ ) {
+	    markersArray[i].setMap(null);
 	  }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-function center() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            map.setCenter(initialLocation);
-        });
-    }
+	  markersArray.length = 0;
 }
 
 google.maps.event.addDomListener(window, 'load', getLocation() );
